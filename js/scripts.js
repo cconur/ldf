aa663f459060e40c5e10762cce1efa5281ff35f9
cconur <98234762+cconur@users.noreply.github.com>
2025-06-08 18:39:39 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -52,9 +52,9 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
     // Activate SimpleLightbox plugin for portfolio items
-    new SimpleLightbox({
-        elements: '#portfolio a.portfolio-box'
-    });
+    // new SimpleLightbox({
+    //     elements: '#portfolio a.portfolio-box'
+    // });
 
 });
 
